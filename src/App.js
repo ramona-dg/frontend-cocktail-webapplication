5a3146969291c647f129021a7f223bc77a83731a
ramona-dg <ramona.rdg@gmail.com>
2022-12-07 12:29:32 +0100

    page components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import Overview from "./pages/overview/Overview";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
 
 function App() {
-  return (
+    return (
+        <>
+            <Home/>
+            <Register/>
+            <Login/>
+            <Profile/>
+            <Search/>
+            <Overview/>
+            <Recipe/>
 
-      <h1>Test Test</h1>
-  );
+        </>
+
+    );
 }
 
 export default App;
---
+++ src/pages/recipe/Recipe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Recipe(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Recipe;
