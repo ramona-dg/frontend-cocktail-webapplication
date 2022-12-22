5e10f807b2811f1a807179241900e1a084ec0d50
ramona-dg <ramona.rdg@gmail.com>
2022-12-22 12:20:39 +0100

    styling homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,7 +10,7 @@ function Home() {
     <>
         <div className={styles['content-container']}>
             <div className={styles.content}>
-           <h1>Lorem ipsum dolor.</h1>
+           <h1>Welcome at Cocktails</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cumque debitis, delectus doloribus eaque expedita fuga harum iusto labore maxime modi nisi nostrum numquam provident quo quos suscipit tempore voluptatum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur consequuntur delectus, dolorem ducimus eligendi, fugit illo iste molestias nemo, perferendis perspiciatis praesentium quasi quo reprehenderit suscipit! Ab accusamus adipisci architecto atque dicta dolorum est et explicabo iste minus nostrum nulla porro, praesentium quia quis quod soluta sunt ullam, velit voluptatem. A ab dolore impedit libero optio quos rem. A animi architecto, assumenda consectetur cum dignissimos error facilis harum laboriosam nemo nisi omnis similique temporibus unde velit? Ab error laborum, odio quasi sint tempore. A alias, aliquid delectus distinctio dolorem enim, est fuga illum, labore magni nemo nulla saepe tenetur unde!</p>
         </div>
             </div>
