b5215d4b13da71982c152f7b8b88e28a3e3868ce
ramona-dg <ramona.rdg@gmail.com>
2023-01-11 13:49:40 +0100

    css layout login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import styles from "../login/Login.module.css";
 
 function Login() {
     return (
-        <div></div>
+        <>
+            <div className={styles.contentContainer}>
+                <div className={`${styles['contentContainer__photo']} ${styles.contentItem}`}>
+                    <p>Hier komt een afbeelding</p>
+                </div>
+                    <div className={`${styles['contentContainer__form']} ${styles.contentItem}`}>
+                    <p>hier komt login form
+                    </p>
+                </div>
+            </div>
+
+        </>
     );
 }
 
