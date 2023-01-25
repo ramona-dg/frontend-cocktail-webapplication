c05a51d725645372fbb4bd842648ddbb5be1b411
ramona-dg <ramona.rdg@gmail.com>
2023-01-25 14:10:28 +0100

    collect data for card component

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -2,38 +2,44 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
 
-function CocktailCard() {
-    const [cocktail, setCocktail] = useState([]);
+function CocktailCard({endpoint}) {
+    const [cocktails, setCocktails] = useState([]);
+
 
     useEffect(() => {
+
         async function fetchCocktails() {
             try {
-                const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=g');
-                console.log(response.data.drinks);
-                setCocktail(response.data.drinks);
+                const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${endpoint}`);
+                setCocktails(response.data.drinks);
             } catch (error) {
                 console.error(error);
             }
         }
-        fetchCocktails();
 
-    }, [])
+        if (endpoint) {
+            fetchCocktails();
+        }
+
+    }, [endpoint]);
 
     return (
+        <section className="cocktail-card">
+            {cocktails && cocktails.map((cocktail) => {
+                return (
+                    <div key={cocktail.idDrink}>
+                        ID: {cocktail.idDrink}
+                        <br/>
+                        Name: {cocktail.strDrink}
+                        <br/>
+                        Alcoholic: {cocktail.strAlcoholic}
+                        <br/>
+                        <br/>
+                    </div>
+                )
+            })}
 
-
-      <section className="cocktail-card">
-          {cocktail &&
-              <>
-                  <h2>{cocktail.strDrink}</h2>
-                  <p><strong>Name: </strong>{cocktail.strDrink}</p>
-                  <p><strong>Weight: </strong>{cocktail.strDrinkThumb}</p>
-                  <p><strong>Alcoholic: </strong>{cocktail.strAlcoholic}</p>
-              </>
-          }
-      </section>
-
-
+        </section>
     );
 }
 
