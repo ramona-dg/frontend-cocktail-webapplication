4e710b3ea57616bd394cb99f0a4612bfe097c5e8
ramona-dg <ramona.rdg@gmail.com>
2023-01-26 12:14:36 +0100

    create search form

--- src/pages/overview/Overview.js
+++ src/pages/overview/Overview.js
@@ -4,7 +4,7 @@ import AbcButton from "../../components/AbcButton";
 
 function Overview() {
     const [letter, setLetter] = useState("a");
-// console.log({letter});
+
     return (<>
        <AbcButton setAbcLetter={setLetter} />
            <CocktailCard endpoint={letter}/>
--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from "../search/Search.module.css";
+import {Link} from "react-router-dom";
 
-function Search(props) {
-    // useParams om dynamische url te kunnen gebruiken voor specifiek product?
+function Search() {
+    const [name, setName] = useState('');
+    const [ingredient, setIngredient] = useState('');
+    const [alcohol, setAlcohol] = useState('');
+    const [category, setCategory] = useState('');
+    const [glass, setGlass] = useState('');
+
+
     return (
-        <div></div>
+        <>
+            <div className={styles.contentContainer}>
+                <div className={`${styles['contentContainer__photo']} ${styles.contentItem}`}/>
+
+                <div className={`${styles['contentContainer__form']} ${styles.contentItem}`}>
+                    <h1>Search</h1>
+                    <form>
+                        <label htmlFor="name-field">
+                            <br/>
+                            <input
+                                name="name"
+                                placeholder={"Cocktail name"}
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+
+                            />
+                        </label>
+                        <br/>
+                        <label htmlFor="ingredient-field">
+                            <br/>
+                            <input
+                                name="ingredient"
+                                placeholder={"Ingredient type"}
+                                type="text"
+                                value={ingredient}
+                                onChange={(e) => setIngredient(e.target.value)}
+                            />
+                        </label>
+                        <br/>
+                        <br/>
+                    </form>
+            {/*hier komen dropdown*/}
+
+                </div>
+            </div>
+
+        </>
     );
 }
 
