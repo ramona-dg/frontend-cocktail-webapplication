ce15a8592bd0c2a7d2c1db8715e6d6ef16ed7045
ramona-dg <ramona.rdg@gmail.com>
2023-01-27 15:09:42 +0100

    fetch data for dropdowns

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -36,6 +36,7 @@ function CocktailCard({endpoint}) {
                         <br/>
                         <br/>
                     </div>
+
                 )
             })}
 
--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -2,30 +2,16 @@ import React, {useState} from 'react';
 import styles from "../search/Search.module.css";
 import Dropdown from "../../components/Dropdown";
 
+
 function Search() {
     const [name, setName] = useState('');
     const [ingredient, setIngredient] = useState('');
-    // const [alcohol, setAlcohol] = useState('');
-    // const [category, setCategory] = useState('');
-    // const [glass, setGlass] = useState('');
-
-    // dit moet straks verplaats en omgebouwd worden tot category, glass type, alcohol
-    const options = [
-        { value: "green", label: "Green"},
-        { value: "yellow", label: "Yellow"},
-        { value: "red", label: "red"},
-        { value: "blue", label: "Blue"},
-
-        {},
-
-    ]
 
 
     return (
         <>
             <div className={styles.contentContainer}>
                 <div className={`${styles['contentContainer__photo']} ${styles.contentItem}`}/>
-
                 <div className={`${styles['contentContainer__form']} ${styles.contentItem}`}>
                     <h1>Search</h1>
                     <form>
@@ -40,25 +26,19 @@ function Search() {
 
                             />
                         </label>
-                        <br/>
-                        <label htmlFor="ingredient-field">
-                            <br/>
-                            <input
-                                name="ingredient"
-                                placeholder={"Ingredient type"}
-                                type="text"
-                                value={ingredient}
-                                onChange={(e) => setIngredient(e.target.value)}
-                            />
-                        </label>
-                        <br/>
+
                         <br/>
                     </form>
                     <br/><br/><br/>
                     {/*hier komen dropdown*/}
-                    <Dropdown isSearchable placeHolder="Alcoholic" options={options}  onChange={(value) => console.log(value)}/>
-                    <Dropdown isSearchable placeHolder="Category" options={options}  onChange={(value) => console.log(value)}/>
-                    <Dropdown isSearchable placeHolder="Glass type" options={options}  onChange={(value) => console.log(value)}/>
+                    <Dropdown isSearchable placeHolder="Ingredient" endpoint="i"
+                              onChange={(value) => console.log(value)}/>
+                    <Dropdown isSearchable placeHolder="Alcoholic" endpoint="a"
+                              onChange={(value) => console.log(value)}/>
+                    <Dropdown isSearchable placeHolder="Category" endpoint="c"
+                              onChange={(value) => console.log(value)}/>
+                    <Dropdown isSearchable placeHolder="Glass type" endpoint="g"
+                              onChange={(value) => console.log(value)}/>
                 </div>
             </div>
 
