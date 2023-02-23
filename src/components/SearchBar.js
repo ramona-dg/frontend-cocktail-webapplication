28a8e9942a0ba739ca032d1dc102013ee72726e5
ramona-dg <ramona.rdg@gmail.com>
2023-02-23 14:54:29 +0100

    full explanation of each step (commented)

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,21 +3,20 @@ import axios from "axios";
 import styles from "./SearchBar.module.css";
 
 
-
 function SearchBar() {
-    const [firstLetter, setFirstLetter] = useState("");
-    const [textInput, setTextInput] = useState("");
-    const [suggestions, setSuggestions] = useState([]);
-    const [cocktails, setCocktails] = useState([]);
+    const [firstLetter, setFirstLetter] = useState(""); // voor de eerste letter om data op te halen voor de cocktails array
+    const [textInput, setTextInput] = useState(""); // input van de gebruiker
+    const [suggestions, setSuggestions] = useState([]); // suggesties die worden verkregen vanuit de onChangeHandler waar de inputwaarde vergeleken wordt met de data van de opgehaalde array
+    const [cocktails, setCocktails] = useState([]); // is de array die verkregen wordt door middel van de eerst ingetypte letter
 
-    // useEffect haalt de array op van de eerst getypte letter die wordt geplaatst in de useState van cocktails
+    // useEffect haalt een array van data op van de eerst getypte letter van de textInput. De data wordt geplaatst in de useState van cocktails.
     useEffect(() => {
         async function fetchList() {
             try {
                 const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${firstLetter}`);
                 console.log(response.data.drinks);
-                setCocktails(response.data.drinks);
-                setSuggestions(response.data.drinks);
+                setCocktails(response.data.drinks); // deze kan volgens mij weg?
+                setSuggestions(response.data.drinks);// Dit is de array waar mogelijke suggesties in zitten in vergelijking met de eerst getypte letter
             } catch (error) {
                 console.error(error);
             }
@@ -27,7 +26,7 @@ function SearchBar() {
 
             fetchList();
         }
-    }, [firstLetter]);
+    }, [firstLetter]); // dependency firstletter, waarbij verandering van firstletter de useEffect opnieuw rendert.
 
     const onSuggestionHandler = (textInput) => {
         setTextInput(textInput);
@@ -35,24 +34,24 @@ function SearchBar() {
     }
 
     const onChangeHandler = (textInput) => {
-        setFirstLetter(textInput.charAt(0).toLowerCase());
-        let matches = [];
+        setFirstLetter(textInput.charAt(0).toLowerCase()); // setten van de eerste letter op de useEffect mee te activeren
 
-        if (textInput.length > 0) {
+        let matches = []; // lege array om mogelijke matches mee in op te vangen
+
+        if (textInput.length > 0) { // Het maken van de matches in overeenkomend met de ingetypte waardes bij inputText
             matches = cocktails.filter((cocktail) => {
-                const regex = new RegExp(`${textInput}`, "gi");
+                const regex = new RegExp(`${textInput}`, "gi"); // hoe dit werkte nog even een keer goed opzoeken.
                 return cocktail.strDrink.match(regex);
             })
         }
-        console.log(matches);
-        setSuggestions(matches);
-        setTextInput(textInput);
-        console.log(textInput);
+        console.log(matches); // loggen van de matches in de console
+        setSuggestions(matches); //setten de overeengekomen matches in vergelijking van de inputText
+        setTextInput(textInput); //setten van de inputText in uitvoering bij onChange
+        console.log(textInput); // loggen van de inputText in de console
     }
 
-    const handleSearch = () => {
-        console.log("aap");
-// Hier moet een doorverwijzing plaats vinden naar de waardes waar een search op gedaan wordt. Een overview van gevonden resultaten naar aanleiding van de search
+    const handleSearch = () => { //// Hier moet een doorverwijzing plaats vinden naar de waardes waar een search op gedaan wordt. Een overview van gevonden resultaten (< geldt alleen voor dropdowns) naar aanleiding van de search of een direct recept vanuit de search balk (deze pagina dus!)
+        console.log("aap");// loggen om te kijken of hij werkt
     }
 
     return (
@@ -62,18 +61,20 @@ function SearchBar() {
                 <label htmlFor="name-field">
                     <br/>
                     <input
-                        placeholder={!textInput ? "Type here your cocktail" : {textInput}}
+                        placeholder={!textInput ? "Type here your cocktail" : {textInput}} // Wat er in de search balk komt te staan
                         type="text"
-                        value={textInput}
-                        onChange={(event) => {
+                        value={textInput} // de waarde wat in de searchbalk staat
+                        onChange={(event) => { // de waarde wat er veranderd in de balk wordt als waarde doorgegeven bij elke verandering (onChange) (verandering.losgelaten.opdeWaarde)
                             onChangeHandler(event.target.value);
                         }}
                     />
 
-                    {suggestions && suggestions.slice(0, 5).map((suggestions, i) =>
-                    <div key={i} className={styles.suggestions} onClick={() =>onSuggestionHandler(suggestions.strDrink)}>
-                        {suggestions.strDrink}
-                    </div>
+                    {suggestions && suggestions.slice(0, 5).map((suggestion, i) => //als er suggesties zijn voer een mapping uit voor de eerste 5 waardes en laat deze waardes als de volgende <div> zien. Uit array suggestions wordt 1 suggestion gehaalf en i indexnummer als ker meegegeven voor de <div>
+                        <div key={i} className={styles.suggestions}
+                             onClick={() => onSuggestionHandler(suggestion.strDrink)}>
+                            {/*bij het aanklikken van de suggestiewaarde voer functie onSuggestionHandler uit met de meegegeven waarde die wordt aangeklikt (suggestion.strDrink*/}
+                            {suggestion.strDrink} {/*de waarde*/}
+                        </div>
                     )}
 
                 </label>
