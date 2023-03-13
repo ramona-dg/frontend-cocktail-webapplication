ed2410570b232a5c12a9ab7fe97e6ba39434c3b6
ramona-dg <ramona.rdg@gmail.com>
2023-03-13 11:25:17 +0100

    fetch data by textInput from SearchBar.js

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,44 +1,50 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from "react-router-dom";
 import axios from "axios";
-import {useLocation} from "react-router-dom";
+
 
 
 // Recipe moet gemaakt worden vanuit search.js ( dropdown + searchbar) en vanuit overview.js (hier moet een onClick komen op de naam van de cokctail)
 
 
-// useParams om dynamische url te kunnen gebruiken voor specifiek product
-function Recipe(textInput) {
-    const navigate = useNavigate();
-    const data = navigate.location.state.data;
+// useParams om dynamische url te kunnen gebruiken voor specifiek product, Is dit nodig?
+function Recipe() {
+ const location = useLocation();
+ const { textInput } = location.state || {}; // Dit lege object zorgt ervoor dat er geen crash ontstaat als de location.state leeg is of undefined. Zou kunnen afhandelen om de button disabled te maken in searchBar.js maar dan moet er ook voor gezorgd worden dat de state niet null of undefined is ipv alleen leeg.
 
 console.log(textInput);
-    const [recipeCocktail, setRecipeCocktail] = useState();
+const [dataCocktail, setDataCocktail] = useState({})
 
-// console.log(textInput);
-//     useEffect(() => {
-//         async function fetchCocktail() {
-//             try {
-//                 const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${textInput}`);
-//                 console.log(response);
-//                 setRecipeCocktail(response); // deze kan volgens mij weg?
-//
-//             } catch (error) {
-//                 console.error(error);
-//             }
-//         }
-//
-//         if (textInput) {
-//
-//             fetchCocktail();
-//         }
-//     }, [textInput]);
+    useEffect(() => {
+        async function fetchCocktail() {
+            try {
+                const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${textInput}`);
+                console.log(response.data.drinks[0]);
+                setDataCocktail(response.data.drinks[0]);
 
+            } catch (error) {
+                console.error(error);
+            }
+        }
 
+        if (textInput) {
+
+            fetchCocktail();
+        }
+    }, [textInput]);
+
+console.log(dataCocktail);
 
 
 // moeten nog textInput waarde uit search zien te krijgen
     return (
-        <div></div>
+
+
+        <>
+            <h3>{dataCocktail.strDrink}</h3>
+            <img  src={dataCocktail.strDrinkThumb} alt="Cocktail Image" />
+
+        </>
     );
 }
 
