797aa844116a079f7b681c2648b742492ff0606f
ramona-dg <ramona.rdg@gmail.com>
2023-03-23 12:09:10 +0100

    authorisation problem when URL/overvieuw, need block when not enter login

--- src/pages/overview/Overview.js
+++ src/pages/overview/Overview.js
@@ -1,13 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import CocktailCard from "../../components/CocktailCard";
 import AbcButton from "../../components/AbcButton";
+import {AuthContext} from "../../context/AuthContext";
 
 // Sommige letter bevatten geen cocktails, zoals de X. Ik kan hier een bericht voor weergeven.
 // Dit kan door in CocktailCard.js een vergelijking met === 0 geef dan blabla weer
 
 function Overview() {
+    const { isAuth } = useContext(AuthContext);
+    // is Auth is om te checken of hij ingelogd weer geeft wanneer de webadres + /search wordt ingevoerd.
+    // dit is dus een fout en moet worden opgelost. Uitzoeken hoe?
     const [letter, setLetter] = useState("a");
 
+
+    console.log(isAuth);
     return (
         <>
             <AbcButton setAbcLetter={setLetter}/>
